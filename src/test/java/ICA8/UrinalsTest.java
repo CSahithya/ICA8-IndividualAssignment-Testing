cd1bc8b5d320988181dfb7cf9af1cb49d729fae0
Sahithya Cherukuri <46041721+SahithyaCherukuri@users.noreply.github.com>
2022-10-27 15:59:55 -0700

    Null String Test

--- src/test/java/ICA8/UrinalsTest.java
+++ src/test/java/ICA8/UrinalsTest.java
@@ -3,20 +3,27 @@ package ICA8;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UrinalsTest {
     static Urinals maleUrinals = new Urinals();
-    public static final String DATAFILE = "";
+    public static final File DATAFILE = new File("");
 
     @BeforeAll
     static void setUpTest(){
-
+        //maleUrinals.openFile(DATAFILE);
     }
 
     @Test
     void checkIfGoodString(){
         assertFalse(maleUrinals.goodString("Sahithya"));
+    }
+
+    @Test
+    void checkStringNotNull(){
+        assertFalse(maleUrinals.goodString(""));
     }
 
 
